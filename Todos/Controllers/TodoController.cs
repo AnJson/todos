10065b19ebeeba1577483cee18b041ddf93fc314
Anders Jonsson <aj224rj@student.lnu.se>
2022-10-24 20:42:25 +0200

    Add Todo class and initial controller

---
+++ Todos/Controllers/TodoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Todos.Model;
+
+namespace Todos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ILogger<TodoController> _logger;
+
+        public TodoController(ILogger<TodoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetTodos")]
+        public IEnumerable<Todo> Get()
+        {
+            return Enumerable.Empty<Todo>(); // Fix this.
+        }
+    }
+}
--- Todos/Model/Todo.cs
+++ Todos/Model/Todo.cs
@@ -1,6 +1,18 @@
-﻿namespace Todos.Model
+﻿using System.Text.Json;
+
+namespace Todos.Model
 {
     public class Todo
     {
+        public string title { get; set; }
+        public string description { get; set; }
+
+        public Todo(String title, String description)
+        {
+            this.title = title;
+            this.description = description;
+        }
+
+        public override string ToString() => JsonSerializer.Serialize<Todo>(this);
     }
 }
