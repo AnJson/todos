a624b7916f3b1d63a250319135c987c44e5ff91c
Anders Jonsson <aj224rj@student.lnu.se>
2022-10-25 19:01:59 +0200

    Add temporary methods for auth

--- Todos/Controllers/AuthController.cs
+++ Todos/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
 using Todos.Model.Auth;
 
 namespace Todos.Controllers
@@ -13,7 +14,35 @@ namespace Todos.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<User>> Register(UserDto request)
         {
-            // Implement.
+            CreatePasswordHash(request.password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.Username = request.username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            return Ok(user);
+        }
+
+        [HttpPost(Name = "login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            if (user.Username != request.username)
+            {
+                return BadRequest("User not found");
+            } else
+            {
+                return Ok("Success!");
+            }
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA1();
+
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
--- Todos/Model/Auth/UserDto.cs
+++ Todos/Model/Auth/UserDto.cs
@@ -2,5 +2,7 @@
 {
     public class UserDto
     {
+        public string username { get; set; }
+        public string password { get; set; }
     }
 }
